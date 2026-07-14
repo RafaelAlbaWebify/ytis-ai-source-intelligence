bde355e8541510989ab6cb23cd77e33702004c20
Rafael Alba <ralbas.int@gmail.com>
2026-07-14 11:29:13 +0200

    Expose technical research package API

---
+++ src/ytis/research/__init__.py
@@ -0,0 +1,17 @@
+from ytis.research.models import (
+    EvidenceUnit,
+    Finding,
+    Investigation,
+    ReviewDecision,
+    SourceDocument,
+)
+from ytis.research.service import TechnicalResearchService
+
+__all__ = [
+    "EvidenceUnit",
+    "Finding",
+    "Investigation",
+    "ReviewDecision",
+    "SourceDocument",
+    "TechnicalResearchService",
+]
